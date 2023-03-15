68212de04679536ef4ce0c06a81037282855c587
ajaoooluseyi <92797727+ajaoooluseyi@users.noreply.github.com>
2023-03-15 20:32:33 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, request, jsonify, make_response #imports Flask web module
 from flask_sqlalchemy import SQLAlchemy  #imports SQLAlchemy module to connect to database
+from flask_restful import Resource, Api
 from datetime import datetime
-from marshmallow import fields
-from marshmallow_sqlalchemy import SQLAlchemySchema, auto_field #imports modules to serialize Python objects
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema #imports modules to serialize Python objects
 
 
 app = Flask(__name__)
+api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 db = SQLAlchemy(app)
 
@@ -20,89 +21,99 @@ class Todo(db.Model):
             return f"{self.text} | status - {self.complete}"
 
 #definimg class to return JSON from python objects
-class TodoSchema(SQLAlchemySchema):
-    class Meta(SQLAlchemySchema.Meta):
+class TodoSchema(SQLAlchemyAutoSchema):
+    class Meta(SQLAlchemyAutoSchema.Meta):
         model =  Todo
-        sqla_session = db.session
-
-    id = fields.Number(dump_only = True)
-    text = fields.String(required = True)
-    complete = fields.Boolean(required = True)
-    date_created = fields.DateTime(dump_only = True)
+        load_instance = True
 
 #GET endpoint
-@app.route('/todo', methods = ['GET'])
-def read():
-    get_list = Todo.query.all()  # queries all the todos from database
-    encoder = TodoSchema(many = True)
-    todos = encoder.dump(get_list)   # serializes objects from SQLAlchemy
+class GetToDo(Resource):
+    def get(self):
+        get_list = Todo.query.all()  # queries all the todos from database
+        encoder = TodoSchema(many = True)
+        todos = encoder.dump(get_list)   # serializes objects from SQLAlchemy
 
-    return make_response(jsonify({'todos':todos})) #returns list of todos as JSON
+        return make_response(jsonify({'todos':todos})) #returns list of todos as JSON
 
-@app.route('/todo/<id>', methods=['GET'])
-def read_by_id(id):
-    get_todo = Todo.query.get(id) # queries all the todos from database by given id
-    encoder = TodoSchema()
-    todo = encoder.dump(get_todo)
-    return make_response(jsonify({"todo": todo}))
+class GetToDoById(Resource):
+    def get(self, id):
+        get_todo = Todo.query.get(id) # queries all the todos from database by given id
+        encoder = TodoSchema()
+        todo = encoder.dump(get_todo)
 
-#POST endpoint
-@app.route('/todo', methods = ['POST'])
-def create_todo():
-    data = request.get_json()  # gets data from request body
-    complete = data['complete']
-    text = data['text']
+        return make_response(jsonify({"todo": todo}))
 
-    new_task = Todo(text=text, complete = complete)
-    db.session.add(new_task) # adds task to database
-    db.session.commit() # commits changes to database
+class AddTask(Resource):
+    def post(self):
+        if request.is_json:
+            data = request.get_json()  # gets data from request body
+            complete = data["complete"]
+            text = data["text"]
 
-    encoder = TodoSchema()
-    todo = encoder.load(data)
-    task = encoder.dump(todo) # creates new task todo
+            new_task = Todo(text=text, complete = complete)
+            db.session.add(new_task) # adds task to database
+            db.session.commit() # commits changes to database
 
-    print(task)
-    return make_response(jsonify({'todo':task}), 200)
+            encoder = TodoSchema()
+            task = encoder.dump(new_task)
 
-#PUT endpoint
-@app.route('/todo/<id>/', methods = ['PUT'])
-def update(id):
-    data = request.get_json() # gets data from request body
-    get_todo = Todo.query.get(id) # queries all the todos from database by given id
 
-    if data['text']:
-        get_todo.text = data["text"] #updates text with new text
+            return make_response(jsonify({'todo':task}), 200)
+        else:
+            return {'error': 'Request not JSON'}, 400
 
-    db.session.add(get_todo) # adds task to database
-    db.session.commit() # commits changes to database
-    encoder = TodoSchema(only=['id', 'text', 'complete']) # serializes objects for SQLAlchemy
-    todo = encoder.dump(get_todo)
-    return make_response(jsonify({'todo':todo}))
+class UpdateTask(Resource):
+    def put(self, id):
+        if request.is_json:
+            data = request.get_json() # gets data from request body
+            get_todo = Todo.query.get(id) # queries all the todos from database by given id
 
-#DELETE endpoint
-@app.route('/todo/<id>/', methods = ['DELETE'])
-def delete(id):
-    todo = Todo.query.get(id)  # queries all the todos from database by given id
-    db.session.delete(todo) # deletes task from database
-    db.session.commit()  # commits changes to database
-    return make_response("", 204)
+            if get_todo is None:
+                return  {'error': "Not found"}, 400
+            else:
+                get_todo.text = data["text"] #updates text with new text
 
-#PATCH endpoint
-@app.route('/todo/<id>/', methods = ['PATCH'])
-def mark_task(id):
-    get_todo = Todo.query.get(id) # queries all the todos from database by given id
-    data = request.get_json()
+                db.session.commit() # commits changes to database
+                return 'Updated', 200
+        else:
+            return {"error": "Request is not JSON"}, 400
 
-    get_todo.complete = data['complete']
-    print(get_todo)
-    db.session.add(get_todo) # adds task to database
-    db.session.commit() # commits changes to database
+class ChangeTaskStatus(Resource):
+    def patch(self, id):
+        if request.is_json:
+            get_todo = Todo.query.get(id) # queries all the todos from database by given id
+            data = request.get_json()
 
-    encoder = TodoSchema(only=['id', "text", 'complete']) # serializes objects for SQLAlchemy
-    todo = encoder.dump(get_todo)
+            if get_todo is None:
+                return  {'error': "Not found"}, 400
+            else:
+                get_todo.complete = data["complete"] #updates text with new text
+                db.session.commit() # commits changes to database
 
-    return make_response(jsonify({'todo':todo}))
+                return 'Updated', 200
+        else:
+            return {'error':"Request is not JSON"}
+
+class DeleteTask(Resource):
+    def delete(self, id):
+        todo = Todo.query.get(id)  # queries all the todos from database by given id
+        if todo is None:
+            return  {'error': "Not found"}, 400
+        else:
+            db.session.delete(todo) # deletes task from database
+            db.session.commit()  # commits changes to database
+            return 'Task deleted', 200
+
+api.add_resource(GetToDo, "/")
+api.add_resource(GetToDoById, "/<int:id>")
+api.add_resource(AddTask, "/")
+api.add_resource(UpdateTask, "/<int:id>")
+api.add_resource(ChangeTaskStatus, "/<int:id>")
+api.add_resource(DeleteTask, "/<int:id>")
+
+
+
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8001)
